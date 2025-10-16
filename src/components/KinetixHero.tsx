bd705f1fddd2fb990a79cebaa89450d1093696b3
KinetixZone <kinetix.zone@gmail.com>
2025-10-16 15:00:09 -0600

    Update KinetixHero.tsx

--- src/components/KinetixHero.tsx
+++ src/components/KinetixHero.tsx
@@ -27,7 +27,7 @@ export default function KinetixHero() {
         <img
           src="/LOGO_KINETIX_NUEVO.png"
           alt="Kinetix Logo"
-          className="w-96 sm:w-72 md:w-80 lg:w-[22rem] xl:w-[26rem] 2xl:w-[30rem] h-auto mb-10"
+          className="w-98 sm:w-72 md:w-80 lg:w-[22rem] xl:w-[26rem] 2xl:w-[30rem] h-auto mb-10"
         />
 
         <h1 className="text-4xl md:text-6xl font-extrabold leading-tight text-white">
@@ -35,7 +35,7 @@ export default function KinetixHero() {
           <span className="text-[#4688ff] block">Functional Zone</span>
         </h1>
 
-        <p className="mt-4 text-lg md:text-xl text-white max-w-xl">
+        <p className="mt-2 text-lg md:text-xl text-white max-w-xl">
           Entrena en el lugar donde la <span className="font-bold">disciplina</span> se encuentra con la{" "}
           <span className="font-bold">fuerza</span>. Transforma tu cuerpo con entrenamiento funcional.
         </p>
