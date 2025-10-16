dd6a647ae1785f67a624d768b218b9229d1b9a21
KinetixZone <kinetix.zone@gmail.com>
2025-10-16 15:34:12 -0600

    Update KinetixHero.tsx

--- src/components/KinetixHero.tsx
+++ src/components/KinetixHero.tsx
@@ -28,8 +28,8 @@ export default function KinetixHero() {
 
 
 
-<img src="/LOGO_KINETIX_NUEVO.png" alt="Kinetix Logo" className="w-80 sm:w-80 md:w-96 lg:w-[24rem] xl:w-[28rem] 2xl:w-[32rem] h-auto mb-6" />` 
-
+<img src="/LOGO_KINETIX_NUEVO.png" alt="Kinetix Logo" className="w-64 sm:w-72 md:w-80 lg:w-[22rem] xl:w-[26rem] 2xl:w-[30rem] h-auto mb-6"
+        />
 
         
         <h1 className="text-4xl md:text-6xl font-extrabold leading-tight text-white">
